643ba64e85ce26dfd2c84a24a8b7ced9e70abc3f
robertferrei <ferreirarobert727@gmail.com>
2024-02-19 21:53:51 -0300

    cadastroPython

---
+++ cadastro.py
@@ -0,0 +1,26 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+#layout tela
+sg.theme('Reddit') #tema
+
+layout =[
+    
+    [sg.Text("usuario"),sg.Input(key=("usuario"))],
+    [sg.Text("Senha"),sg.Input(key="senha",password_char="*")],
+    [sg.Checkbox("salvar login?")],
+    [sg.Button("Entrar")]
+]
+
+#janela
+
+janela = sg.Window("tela de login",layout)
+
+#ler eventos
+
+while True:
+   eventos, valores= janela.read()
+   if eventos == sg.WINDOW_CLOSED:
+       break
+   if eventos == "Entrar":
+       if valores ["usuario"] == "robert" and valores["senha"] == "12345":
+           print("bem vindo ")
